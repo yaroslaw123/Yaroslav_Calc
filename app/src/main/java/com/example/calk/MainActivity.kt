4084e9c12d6bb3e5de8e38e58332c0d435e86986
Charkos0ff <charkosoff@gmail.com>
2021-03-23 14:40:12 +0700

    Initial commit

---
+++ app/src/main/java/com/example/calk/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.calk
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        findViewById<Button>(R.id.textView).setOnClickListener{
+            var yjkm = 0
+            var jlby = 1
+            var ldf = 2
+            var nhb = 3
+            var xtnsht = 4
+            var gznm = 5
+            var itcnm = 6
+            var ctvm = 7
+            var djctvm = 8
+            var ltdznm = 9
+            var pfgznfz = (",")
+        }
+    }
+}
